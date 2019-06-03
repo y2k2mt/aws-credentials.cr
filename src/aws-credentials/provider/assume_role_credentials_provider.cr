e22692c9e9f36c4966f510ec970fb9d11ab9870c
y2k2mt <y2.k2mt@gmail.com>
2019-06-03 17:03:41 +0900

    More tests in AWS environment

--- src/aws-credentials/provider/assume_role_credentials_provider.cr
+++ src/aws-credentials/provider/assume_role_credentials_provider.cr
@@ -8,13 +8,13 @@ module Aws::Credentials
       @role_arn : String,
       @role_session_name : String,
       @sts_client : STSClient,
-      @duration_seconds : Time::Span? = nil,
+      @duration : Time::Span? = nil,
       @policy : JSON::Any? = nil
     )
     end
 
     def credentials : Credentials
-      @sts_client.assume_role(@role_arn, @role_session_name, @duration_seconds, @policy)
+      @sts_client.assume_role(@role_arn, @role_session_name, @duration, @policy)
     end
   end
 end
--- src/aws-credentials/sts_client.cr
+++ src/aws-credentials/sts_client.cr
@@ -1,3 +1,4 @@
+require "json"
 require "./http_client"
 require "./xml"
 
@@ -6,30 +7,33 @@ module Aws::Credentials
     def initialize(
       @contractor_credential_provider : Provider,
       @signer : Proc(HTTP::Request, Credentials, HTTP::Request),
-      @endpoint : String = "https://sts.amazonaws.com"
+      region : String? = nil,
+      @endpoint : String = region ? "https://sts.#{region}.amazonaws.com" : "https://sts.amazonaws.com"
     )
     end
 
     def assume_role(
       @role_arn : String,
       @role_session_name : String,
-      @duration_seconds : Time::Span?,
-      @policy : JSON::Any?
+      @duration : Time::Span? = nil,
+      @policy : JSON::Any? = nil
     ) : Credentials
       param = HTTP::Params.build { |form|
+        form.add "Version", "2011-06-15"
         form.add "Action", "AssumeRole"
         form.add "RoleSessionName", @role_session_name
         form.add "RoleArn", @role_arn
         @policy.try { |p|
           form.add "Policy", p.to_json
         }
-        @duration_seconds.try { |d|
+        @duration.try { |d|
           form.add "DurationSeconds", d.total_seconds.to_i64.to_s
         }
         form
       }
       endpoint_uri = URI.parse @endpoint
       request = HTTP::Request.new("GET", endpoint_uri.path || "/")
+      request.headers["Host"] = endpoint_uri.host.not_nil!
       request.query = param
       signed_request = @signer.call(request, @contractor_credential_provider.credentials)
       response = HTTPClient.exec endpoint_uri, signed_request
@@ -44,7 +48,7 @@ module Aws::Credentials
           expiration: Time.parse_iso8601(xml.string("#{credentials_xml_root}/Expiration")),
         )
       else
-        raise MissingCredentials.new "Failed to assume role via sts : #{response.status_code} : #{response.body}"
+        raise "Failed to assume role via sts : #{response.status_code} : #{response.body}"
       end
     rescue e
       raise MissingCredentials.new e
